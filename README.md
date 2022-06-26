ad27be7c07db6f597bf42c944951ee1aba0d0e92
akakss225 <akakss225@naver.com>
2022-06-26 13:18:22 +0900

    build

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # MPR
+
 - 간단한 Notion 기획서 URL 👇 <br>
-https://magical-fluorine-7a0.notion.site/MPR-595558d0c4ea48648ab31f5f72bbff7a
+  https://magical-fluorine-7a0.notion.site/MPR-595558d0c4ea48648ab31f5f72bbff7a
 
 <hr>
---
+++ src/test/java/com/mpr/backend/MprBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpr.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MprBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
